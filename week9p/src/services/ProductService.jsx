d00db0e33469d82d53440656b84b87e32d666e7e
Vedhanth Sinde <vedhanthsindhe8@gmail.com>
2026-09-22 09:25:09 +0530

    week9p

---
+++ week9p/src/services/ProductService.jsx
@@ -0,0 +1,6 @@
+import axios from 'axios';
+const API_URL = "http://localhost:8085/api/products";
+export const getAllProducts = () => axios.get(API_URL);
+export const addProduct = (product) => axios.post(API_URL, product);
+export const updateProduct = (id, product) => axios.put(`${API_URL}/${id}`, product);
+export const deleteProduct = (id) => axios.delete(`${API_URL}/${id}`);
